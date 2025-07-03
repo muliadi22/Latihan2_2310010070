12d5486a70875dabef91c3227610c3b19fbeede2
muliadi22 <imuly26@gmail.com>
2025-07-03 13:36:36 +0800

    membuat objek

---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,9 @@
+package latihan2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        DataDiri saya = new DataDiri("Nabil", "2310010123", "TI-4B");
+        
+        saya.tampilkanInfo();
+    }
+}
